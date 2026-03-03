dfb2c2fcfa29b50a5dcb0d132ac9d6ea71bc01e5
khairulazanni <khairul.azanni@gmail.com>
2026-03-03 11:51:07 +0800

    Lesson 2: Ecapsulation

--- JavaLearning/src/main/java/com/khairulazanni/Main.java
+++ JavaLearning/src/main/java/com/khairulazanni/Main.java
@@ -1,17 +1,12 @@
 package com.khairulazanni;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        var employee = new Employee();
+        employee.baseSalary = 50000;
+        employee.hourlyRate = 20;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        int wage = employee.calculateWage(20);
+        System.out.println("Employee's wage is: " + wage);
     }
 }
