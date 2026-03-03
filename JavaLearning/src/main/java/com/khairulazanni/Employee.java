c35411f01430221187d7bc1572c874b85a6ff1b5
khairulazanni <khairul.azanni@gmail.com>
2026-03-03 12:31:14 +0800

    Lesson 3: Getter Setter

--- JavaLearning/src/main/java/com/khairulazanni/Employee.java
+++ JavaLearning/src/main/java/com/khairulazanni/Employee.java
@@ -1,8 +1,34 @@
 package com.khairulazanni;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+
+
+    private int hourlyRate;
+
+    private void checkValue(int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("Salary cannot be 0 or less");
+        }
+    }
+
+    public void setBaseSalary(int baseSalary){
+        checkValue(baseSalary);
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate){
+        checkValue(hourlyRate);
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
 
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
--- JavaLearning/src/main/java/com/khairulazanni/Main.java
+++ JavaLearning/src/main/java/com/khairulazanni/Main.java
@@ -3,8 +3,8 @@ package com.khairulazanni;
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50000;
-        employee.hourlyRate = 20;
+        employee.setBaseSalary(6600);
+        employee.setHourlyRate(10);
 
         int wage = employee.calculateWage(20);
         System.out.println("Employee's wage is: " + wage);
